2da23886f7148c8e388ecbce8b20dbdc41bf4186
Jeff Cailteux <jeff@ubuntu.(none)>
2013-02-13 19:32:51 -0800

    first push

---
+++ quash.c
@@ -0,0 +1,15 @@
+#include <stdlib.h>i
+#include <stdio.h>
+#include <unistd.h>
+#include <strings.h>
+#include <errno.h>
+#include <sys/wait.h>
+#include <string.h>
+
+int main(int argc, char **argv, char **envp){
+
+
+
+
+  return 0;
+}
